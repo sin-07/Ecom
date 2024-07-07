94e501e23df58f53168e992ee9d98fd40df207f4
Aniket singh <aniket.singh9322@gmail.com>
2024-07-08 00:37:20 +0530

    addedFilter

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -6,7 +6,26 @@ const AllProducts = () => {
   const [products, setProducts] = useState([]);
   const [productsCategory, setproductsCategory] = useState([]);
   const [selectProduct, setSelectProduct] = useState("");
-  const [selectTop, setSelectTop] = useState("");
+  const [ShowAllProduct, setShowAllProduct] = useState([]);
+  const [afterselect, setAfterselect] = useState(false);
+
+  // screen pe products show krna hai to..
+  useEffect(() => {
+    const showAllproducts = async () => {
+      try {
+        const res = await axios("https://dummyjson.com/products");
+        setShowAllProduct(res.data.products);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    showAllproducts();
+  }, []);
+
+  {
+    /* products mai kya kya hai */
+  }
 
   useEffect(() => {
     const getAllProducts = async () => {
@@ -20,12 +39,14 @@ const AllProducts = () => {
     getAllProducts();
   }, []);
 
+  //  jo product hum search kar chuke hai usko screen par show karne ke liye
   const filterProducts = (allItems) => {
-    console.log(allItems);
+    setAfterselect(true)
     setSelectProduct(allItems);
     
   };
- 
+
+  // ek products mai kitni company ki variety hai wo hume category se milega
 
   useEffect(() => {
     const getAllProductsCategory = async () => {
@@ -49,39 +70,74 @@ const AllProducts = () => {
     <>
       <Layout>
         <div className="min-h-screen">
+          {/* // products mai kya kya hai */}
           <div className="flex flex-wrap justify-center gap-1">
-            {products.map((allItems, i) => (
-              <div key={i} className="">
-                <button
-                  className="bg-black text-white px-3 py-2 rounded-md my-1"
-                  onClick={() => filterProducts(allItems)}
+            <select
+              name=""
+              id=""
+              onChange={(e) => filterProducts(e.target.value)}
+            >
+              <option>Search products</option>
+              {products
+                .filter(
+                  (filterItems) =>
+                    ![
+                      "motorcycle",
+                      "skin-care",
+                      "kitchen-accessories",
+                    ].includes(filterItems)
+                )
+                .map((allItems, i) => (
+                  <option key={i} value={allItems}>
+                    {allItems}
+                  </option>
+                ))}
+            </select>
+          </div>
+          {/* // ek products mai kitni company ki variety hai wo hume
+          productsCategory se milega */}
+          {afterselect ? (
+            <div className="flex flex-wrap justify-center text-center gap-2 mt-2">
+              {productsCategory.map((allCategory, i) => (
+                <div
+                  key={i}
+                  className="bg-slate-50 rounded-t-2xl hover:shadow-xl transition-all duration-500 ease-in-out overflow-hidden"
                 >
-                  {allItems}
-                </button>
-              </div>
-            ))}
-          </div>
+                  <img
+                    src={allCategory.thumbnail}
+                    alt=""
+                    className="object-cover bg-black object-center w-full block"
+                  />
 
-          <div className="flex flex-wrap justify-center text-center gap-2 mt-2">
-            {productsCategory.map((allCategory, i) => (
-              <div
-                key={i}
-                className="bg-slate-50 rounded-t-2xl hover:shadow-xl transition-all duration-500 ease-in-out overflow-hidden"
-              >
-                <img
-                  src={allCategory.thumbnail}
-                  alt=""
-                  className="object-cover bg-black object-center w-full block"
-                />
-
-                <div className="bg-slate-200 rounded-tl-3xl">
-                  <p>{allCategory.brand}</p>
-                  <p>{allCategory.price}</p>
-                  
+                  <div className="bg-slate-200 rounded-tl-3xl">
+                    <p>{allCategory.brand}</p>
+                    <p>{allCategory.price}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-wrap justify-center gap-2">
+              {ShowAllProduct.map((showingProducts, i) => (
+                <div
+                  key={i}
+                  className="bg-slate-50 rounded-t-2xl hover:shadow-xl transition-all duration-500 ease-in-out overflow-hidden"
+                >
+                  <img
+                    src={showingProducts.thumbnail}
+                    alt=""
+                    className="object-cover bg-black object-center w-full block"
+                  />
+                  <div>
+                    <p>{showingProducts.brand}</p>
+                    <p>{showingProducts.price}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+
+          {/* screen pe products dikhane k liye */}
         </div>
       </Layout>
     </>
