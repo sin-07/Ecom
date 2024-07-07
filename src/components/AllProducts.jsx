92d9ee8a4979291bc871a642c78dcf5eb219f9f3
Aniket singh <aniket.singh9322@gmail.com>
2024-07-07 21:56:43 +0530

    addedSearchFunctionality

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,12 +1,91 @@
-import React from 'react'
-import Layout from './Layout'
+import React, { useEffect, useState } from "react";
+import Layout from "./Layout";
+import axios from "axios";
 
-function AllProducts() {
+const AllProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [productsCategory, setproductsCategory] = useState([]);
+  const [selectProduct, setSelectProduct] = useState("");
+  const [selectTop, setSelectTop] = useState("");
+
+  useEffect(() => {
+    const getAllProducts = async () => {
+      try {
+        const res = await axios("https://dummyjson.com/products/category-list");
+        setProducts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllProducts();
+  }, []);
+
+  const filterProducts = (allItems) => {
+    console.log(allItems);
+    setSelectProduct(allItems);
+    
+  };
+ 
+
+  useEffect(() => {
+    const getAllProductsCategory = async () => {
+      try {
+        if (selectProduct) {
+          const res = await axios(
+            `https://dummyjson.com/products/category/${selectProduct}`
+          );
+          setproductsCategory(res.data.products);
+          console.log(res.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAllProductsCategory();
+  }, [selectProduct]);
+
   return (
-    <Layout>
-        
-    </Layout>
-  )
-}
+    <>
+      <Layout>
+        <div className="min-h-screen">
+          <div className="flex flex-wrap justify-center gap-1">
+            {products.map((allItems, i) => (
+              <div key={i} className="">
+                <button
+                  className="bg-black text-white px-3 py-2 rounded-md my-1"
+                  onClick={() => filterProducts(allItems)}
+                >
+                  {allItems}
+                </button>
+              </div>
+            ))}
+          </div>
 
-export default AllProducts
+          <div className="flex flex-wrap justify-center text-center gap-2 mt-2">
+            {productsCategory.map((allCategory, i) => (
+              <div
+                key={i}
+                className="bg-slate-50 rounded-t-2xl hover:shadow-xl transition-all duration-500 ease-in-out overflow-hidden"
+              >
+                <img
+                  src={allCategory.thumbnail}
+                  alt=""
+                  className="object-cover bg-black object-center w-full block"
+                />
+
+                <div className="bg-slate-200 rounded-tl-3xl">
+                  <p>{allCategory.brand}</p>
+                  <p>{allCategory.price}</p>
+                  
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default AllProducts;
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import g1 from "../../public/g1.jpg";
-import g2 from "../../public/g2.jpg";
-import g3 from "../../public/g3.jpg";
-import g4 from "../../public/g4.jpg";
-import g5 from "../../public/g5.jpg";
-import g6 from "../../public/g6.jpg";
+import g1 from "/g1.jpg";
+import g2 from "/g2.jpg";
+import g3 from "/g3.jpg";
+import g4 from "/g4.jpg";
+import g5 from "/g5.jpg";
+import g6 from "/g6.jpg";
 
 function Gallery() {
   return (
@@ -13,7 +13,7 @@ function Gallery() {
         <div class="container px-5 py-20 mx-auto flex flex-wrap">
           <div class="flex flex-wrap md:-m-2 -m-1">
             <div class="flex flex-wrap w-1/2">
-              <div classname="md:p-2 p-1 w-1/2">
+              <div class="md:p-2 p-1 w-1/2">
                 <img
                   alt="gallery"
                   class="w-full object-cover h-full object-center block hover:scale-75 hover:translate-x-4 hover:skew-y-3 transition duration-500"
